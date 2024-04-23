30b0bf8609baa6424219effd811313a4cd018989
Kamrul Jaman <kamrul.jaman26@gmail.com>
2024-04-24 01:13:22 +0600

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.learnscience {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.learnscience to javafx.fxml;
+    exports com.example.learnscience;
+}
