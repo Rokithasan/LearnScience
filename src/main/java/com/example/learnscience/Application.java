54979be3d29bfc2094b6c3645c8930f1aff1e37d
Kamrul Jaman <kamrul.jaman26@gmail.com>
2024-04-26 21:02:02 +0600

    added design

--- src/main/java/com/example/learnscience/Application.java
+++ src/main/java/com/example/learnscience/Application.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("living_and_non_living.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("parts_of_the_body.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 700);
         stage.setTitle("Learn Science");
         stage.setScene(scene);
--- src/main/java/com/example/learnscience/MyFiveSenses.java
+++ src/main/java/com/example/learnscience/MyFiveSenses.java
@@ -1,2 +1,4 @@
-package com.example.learnscience;public class MyFiveSenses {
+package com.example.learnscience;
+
+public class MyFiveSenses {
 }
